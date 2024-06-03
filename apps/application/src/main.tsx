4cf4c72999a200633e9a8a00d322a64eeb78e1ee
Danijel Maksimovic <maksimovic.danijel989@gmail.com>
2024-06-03 16:18:33 +0200

    async worker start

--- apps/application/src/main.tsx
+++ apps/application/src/main.tsx
@@ -4,10 +4,22 @@ import App from './App'
 import './index.css'
 import { worker } from './mocks/browser'
 
-worker.start()
+async function enableMocking() {
+  if (process.env.NODE_ENV !== 'development') {
+    return
+  }
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+  const { worker } = await import('./mocks/browser')
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start()
+}
+
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+})
